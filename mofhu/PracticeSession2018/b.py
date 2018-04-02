9280b6eefd1817953712028e4670374a403b693e
Mo Frank Hu <mofrankhu@gmail.com>
2018-04-02 19:32:55 +0800

    PracticeSession2018: bl update.
    revise of small bug in b. should be able to handle bl (not tested).

--- mofhu/PracticeSession2018/b.py
+++ mofhu/PracticeSession2018/b.py
@@ -8,9 +8,19 @@ def check(n):
     else:
         return False
 
+def printd():
+    print("d = {", )
+    s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(26):
+        print("{}: '{}',".format(i, s[i]))
+    print("}", )
 
-d = {0:"A", 1:"B", 2:"C",
-     3:"D", 4:"E", 5:"F"}
+# printd()
+
+d = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H',
+     8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O',
+     15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V',
+     22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
 
 for case in range(1, ncase+1):
     MAX_GROUP = 26
