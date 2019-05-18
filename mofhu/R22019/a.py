f2b884fb99213fc68a1a114a9013cde0caed2696
Mo Frank Hu <mofrankhu@gmail.com>
2019-05-19 07:45:04 +0800

    R22019: al AC after match.
    see diff for optimization O(n2). need optimization, as after transform scale from
    300 to ~45000.

--- mofhu/R22019/a.py
+++ mofhu/R22019/a.py
@@ -1,4 +1,5 @@
 ncase = int(input())
+from math import gcd
 
 for case in range(1, ncase+1):
     n = int(input())
@@ -10,32 +11,21 @@ for case in range(1, ncase+1):
     m.sort(key=lambda x:x[1])
     m.sort(key=lambda x:x[0])
     # print(m)
-    thresholds = []
+    thresholds = set()
+    # max O(n2, n=300) -> len(thresholds) ~ 45000
+    # use gcd to min(len(thresholds)) and ignore 2nd iteration
     for i in range(n):
         for j in range(i+1,n):
             if m[i][1] > m[j][1]:  # [i][0] will always <= [j][0] by sort
                 if m[i][0] < m[j][0]:
-                    thresholds.append(((m[i][1] - m[j][1]), (m[j][0] - m[i][0])))
+                    x, y = m[i][1] - m[j][1], m[j][0] - m[i][0]
+                    g = gcd(x,y)
+                    x, y = int(round(x/g)), int(round(y/g))
+                    if (x,y) not in thresholds:
+                        thresholds.add((x,y))
     # print(thresholds)
-    thresholds.sort(key=lambda x:x[1])
-    thresholds.sort(key=lambda x:x[0])
     # print(thresholds)
-    t = len(thresholds)
     d = []
-    for i in range(t):
-        for j in range(i+1,t):
-            x = thresholds[i]
-            y = thresholds[j]
-            # print(x,y)
-            if x[0] * y[1] == x[1] * y[0]:
-                d.append(j)
-    d.sort(key=lambda x:0-x)
-    # print(d)
-    d0 = []
-    for i in d:
-        if i not in d0:
-            del thresholds[i]
-        d0.append(i)
     # print(thresholds)
     t = len(thresholds)
     ans = t+1
