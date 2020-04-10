9748bc626fddcd1b656eeaea8bf75d65b7137e14
Mo Frank Hu <mofrankhu@gmail.com>
2020-04-11 06:41:54 +0800

    QR2020: ds1 AC.

---
+++ mofhu/QR2020/d.py
@@ -0,0 +1,15 @@
+t, b = [int(s) for s in input().split(' ')]
+
+# only for test set 1 and validate runner
+
+
+
+for case in range(t):
+    s = []
+
+    for i in range(1, 11):
+        print(i)
+        s.append(input())
+
+    print("".join(s))
+    ans = input()
