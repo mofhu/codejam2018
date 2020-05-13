53146b0b26f197df59c626f293848681ff11ff46
Mo Frank Hu <mofrankhu@gmail.com>
2020-05-13 08:43:41 +0800

    R22019: b AC.

--- mofhu/R22019/b.py
+++ mofhu/R22019/b.py
@@ -1,105 +1,52 @@
 ncase = int(input())
-from math import gcd
+import time
 from math import floor
 
+output = open('b.out', 'w')
+
 for case in range(1, ncase+1):
-    n = int(input())
-    m = []
-    for i in range(n):
-        x, y = (int(s) for s in input().split(" "))
-        m.append((x,y))
-    # print(m)
-    thresholds = []
-    ans = ""
-    flag = False
-    for i in range(n):
-        for j in range(i+1,n):
-            if m[i][0] >= m[j][0] and m[i][1] >= m[j][1]:
-                ans = "IMPOSSIBLE"
-                flag = True
-                break
-            elif m[i][0] < m[j][0] and m[i][1] <= m[j][1]:
-                pass
-            elif m[i][0] <= m[j][0] and m[i][1] < m[j][1]:
-                pass
+    nv = [100 for i in range(20)]
+    final_vase = -1
+    while (True):
+        day = int(input())
+        idx = []
+        if day <= 75:
+            mod = day % 15
+            div = int(floor(day / 15))
+            ans = '{} {}'.format(mod+1, 100-div)
+            print(ans)
+        elif day < 100:
+            mod = day - 75 + 15 - 1
+            if day <= 80:
+                ans = '{} 0'.format(mod+1)
+                print(ans)
+                # output.write(ans+'\n')
+                line = input()
+                nv[mod] = len([s for s in line.split(' ')]) -1
             else:
-                if m[i][0] < m[j][0] and m[i][1] > m[j][1]:
-                    thresholds.append((m[i][0]-m[j][0], m[j][1]-m[i][1]))
-                if m[i][0] > m[j][0] and m[i][1] < m[j][1]:
-                    thresholds.append((m[i][0]-m[j][0], m[j][1]-m[i][1]))
-        if flag == True:
-            break
-
-    print(thresholds)
-    tp = []
-    tn = []
-    t = len(thresholds)
-    if t == 0:
-        ans = "1 1"
-    else:
-        for i in thresholds:
-            if i[0] + i[1] > 0:
-                tp.append(i)
-            else:
-                tn.append(i)
-        d = []
-        flag = thresholds[0][0] + thresholds[0][1]
-        if flag > 0:
-            flag = 1
+                min_value = min(nv)
+                for i in range(15, len(nv)):
+                    if nv[i] == min_value:
+                        idx.append(i)
+                if final_vase == -1:
+                    final_vase = idx[0] + 1
+                    nv[idx[0]] = 100
+                    del idx[0]
+                    if len(idx) == 0:
+                        min_value = min(nv)
+                        for i in range(15, len(nv)):
+                            if nv[i] == min_value:
+                                idx.append(i)
+                mod = idx[0]
+                nv[mod] += 1
+                ans = '{} 100'.format(mod+1)
+                print(ans)
+                # output.write(ans+'end {}\n'.format(final_vase))
         else:
-            flag = -1
-        t = len(tp)
-        for i in range(tp):
-            for j in range(i+1,t):
-                x = tp[i]
-                y = tp[j]
-                # print(x,y)
-                if x[0] * y[1] == x[1] * y[0]:
-                    d.append(j)
-        t = len(tn)
-        for i in range(tn):
-            for j in range(i+1,t):
-                x = tn[i]
-                y = tn[j]
-                # print(x,y)
-                if x[0] * y[1] == x[1] * y[0]:
-                    d.append(j)
-        if ans != "IMPOSSIBLE":
-            d.sort(key=lambda x:0-x)
-            # print(d)
-            d0 = []
-            for i in d:
-                if i not in d0:
-                    del thresholds[i]
-                d0.append(i)
-            # print(thresholds, flag)
-            if flag == -1:
-                thresholds.sort(key=lambda x:x[0]/x[1])
-            else:
-                thresholds.sort(key=lambda x:x[1]/x[0])
-            x, y = abs(thresholds[0][0]), abs(thresholds[0][1])
-            g = gcd(x, y)
-            x, y = int(round(x / g, 0)), int(round(y / g,0))
-            # print(g, x, y, flag)
-            if flag == -1:
-                if y%x == 0:
-                    ans = "{} {}".format(y+1,x)
-                else:
-                    y0 = floor(y/x) + 1
-                    ans = "{} {}".format(y0,1)
-            else:
-                if x%y == 0:
-                    ans = "{} {}".format(1,x+1)
-                else:
-                    y0 = floor(x/y) + 1
-                    ans = "{} {}".format(1,y0)
+            print('{} 100'.format(final_vase))
+            # output.write('final {}\n'.format(final_vase))
+            break
+    time.sleep(0.01)
 
-            # ans = t+1
-            x, y = [int(s) for s in ans.split(" ")]
-            print(x, y)
-            for i,j in m:
-                print(x*i + y*j)
-
-    print("Case #{}: {}".format(case, ans))
 
 
