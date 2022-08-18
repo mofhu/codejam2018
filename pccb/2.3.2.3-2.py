11d90bcef1464093943c6f713d9d38176b75e7b0
Mo Frank Hu <mofrankhu@gmail.com>
2022-08-18 23:56:46 +0800

    pccb: update DP solution of 2.3.2.3.

---
+++ pccb/2.3.2.3-2.py
@@ -0,0 +1,33 @@
+# author: mofhu@github
+
+n = 3
+a = [3, 5, 8]
+m = [3, 2, 2]
+k = 17
+
+# v2 whole sum (m is used)
+am = len(a) + 10
+km = k + am + 10
+
+dp = [[-1 for i in range(km)] for j in range(am)]
+
+for i in range(len(a)):
+    dp[i][0] = 1
+    for j in range(k):
+        # key: remove triple cycle and only cycle with k
+        if dp[i][j] >= 0:
+            dp[i+1][j] = m[i]
+        elif j < a[i] or dp[i+1][j-a[i]] <= 0:
+            dp[i+1][j] = -1
+        else:
+            dp[i+1][j] = dp[i+1][j-a[i]] - 1
+            print(i+1, j-a[i])
+    print(dp[i+1])
+
+print(dp[len(a)][:k+1])
+if dp[len(a)][k] == 0:
+    print('No')
+else:
+    print('Yes')
+
+
