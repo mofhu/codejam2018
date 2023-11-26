f4d49493cba6320b44b58b127db7c73942c29546
Mo Frank Hu <mofrankhu@gmail.com>
2023-11-27 05:56:53 +0800

    ICPC_huawei2023: begin coding.
    - a quick draft thinking.

---
+++ codeforces/ICPC2023OnlineChallenge/a.py
@@ -0,0 +1,11 @@
+# author: mofhu@github
+# A. Deterministic Scheduling for Extended Reality over 5G and Beyond
+
+# earliest attempt:
+"""
+20231127 design quick mvp
+
+just give each frame a score and try to set from highest to lowest.
+
+"""
+
