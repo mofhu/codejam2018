787889ad9b4656edaecdaa0668e26a10d5179bfe
Mo Frank Hu <mofrankhu@gmail.com>
2023-12-03 08:39:58 +0800

    MVP version: get 2841.945 points.

--- codeforces/ICPC2023OnlineChallenge/a.py
+++ codeforces/ICPC2023OnlineChallenge/a.py
@@ -12,26 +12,81 @@ may extend other parameters into score functions in later version.
 e.g. less power; user/freq...
 
 """
-
+# debug = True
+debug = False
 # input
-n = int(input())
-k = int(input())
-t = int(input())
-r = int(input())
+N = int(input())
+K = int(input())
+T = int(input())
+R = int(input())
 sinr = []
-for i in range(r*k*t):
+for i in range(R*K*T):
     line = [float(s) for s in input().split(' ')]
     sinr.append(line)
 d = []
-for i in range(n*r*k):
+for i in range(N*R*K):
     line = [float(s) for s in input().split(' ')]
     d.append(line)
-j = int(input())
+J = int(input())
 frames = []
-for i in range(j):
+for i in range(J):
     line = [int(s) for s in input().split(' ')]
     frames.append(line)
 
-print(sinr)
-print(d)
-print(frames)
+# print(sinr)
+# print(d)
+# print(frames)
+
+# output
+# k_limit = [[0 for n in range(N)]] * R * T  # not implemented in MVP
+p_limit = [min(R, 4) for i in range(T)]  # temp use 4 as max power
+# p_limit = [R for i in range(T)]
+output = []
+for i in range(R*K*T):
+    output.append([0 for i in range(N)][:])
+# print(output)
+if debug:
+    print(p_limit)
+frames.sort(key=lambda x: x[1]/x[4])
+
+
+for f in frames:
+    if debug:
+        print(f[0], p_limit)
+    t0 = f[3]
+    user = f[2]
+    for t in range(t0, t0+f[4]):
+        # check if p_limit[t] is still available
+        if p_limit[t] <= 0:
+            continue
+    best_k = {}
+    # best_p = {}
+    for t in range(t0, t0+f[4]):
+        # find best cell (k) for this frame (t)
+        if p_limit[t] > 0:
+            for r in range(R):
+                # r*k search not implemented in MVP;
+                # only find best k in r*k space and use it for all time
+                for k in range(K):
+                    best_sinr = 0
+                    sinr_t_k = sinr[r + k*R + t*K*R -1][user]  # user = f[2]
+                    if sinr_t_k > best_sinr:
+                        best_sinr = sinr_t_k
+                        best_k[t] = k
+            if debug:
+                print(f't={t}, best_k is {best_k[t]}, best_sinr is {best_sinr}')
+                print(f'bestk {best_k}')
+            best_p = min(p_limit[t], 4)
+            if best_p > 0:
+                p_limit[t] -= best_p
+                # print(f'frame{f[0]}, user{f[2]}, best_sinr {best_sinr}, best_p: {best_p[t]}')  # debug
+                # ignore the calculation of transferred bits in MVP
+            # set best_k to output
+            if t in best_k:
+                k = best_k[t]
+                output[r + k*R + t*K*R -1][user] = best_p
+if debug:
+    print(p_limit)
+# print(output)  # debug
+for i in output:
+    print(' '.join(map(str, i)))
